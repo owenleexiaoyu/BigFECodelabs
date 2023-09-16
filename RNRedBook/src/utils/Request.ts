711706ca962564f58ad3e166ab199263290d4c37
OwenLee <2924354040@qq.com>
2023-09-16 08:13:10 +0800

    axios 添加异常结果拦截器

--- RNRedBook/src/utils/Request.ts
+++ RNRedBook/src/utils/Request.ts
@@ -6,6 +6,34 @@ const instance = axios.create({
     timeout: 10 * 1000,
 });
 
+// 对网络请求的 Response 进行通用的拦截处理
+instance.interceptors.response.use(
+    response => response, 
+    error => {
+        const { response } = error;
+        if (response) {
+            const { status } = response;
+            if (status >= 500) {
+                // 服务端报错
+                console.log("Axios Interceptor ===> 服务端报错");
+            } else if (status === 400) {
+                // 接口参数异常
+                console.log("Axios Interceptor ===> 接口参数异常");
+            } else if (status === 401) {
+                // 登录信息过期，需要重新登录
+                console.log("Axios Interceptor ===> 登录信息过期，需要重新登录");
+            } else {
+                // 其他错误类型，统一按照接口报错处理
+                console.log("Axios Interceptor ===> 其他错误类型，统一按照接口报错处理");
+            }
+        } else {
+            // 网络异常
+            console.log("Axios Interceptor ===> 网络异常");
+        }
+        return Promise.reject(error);
+    }
+);
+
 const get = (url: string, params: any): Promise<AxiosResponse<any, any>> => {
     return instance.get(url, {
         params: params,
